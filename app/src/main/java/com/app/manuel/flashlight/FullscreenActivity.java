de884bf6d34f1c6bc28864ede2eaf0e647ec3f41
manuelkoliqi <Campeador123>
2017-05-09 21:45:05 +0200

    First Commit

---
+++ app/src/main/java/com/app/manuel/flashlight/FullscreenActivity.java
@@ -0,0 +1,25 @@
+package com.app.manuel.flashlight;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class FullscreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+
+
+}
