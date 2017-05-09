c7bfaf286ea79a7b15918a747ea37043008ddd77
manuelkoliqi <Campeador123>
2017-05-10 00:21:44 +0200

    Added images

--- app/src/main/java/com/app/manuel/flashlight/FullscreenActivity.java
+++ app/src/main/java/com/app/manuel/flashlight/FullscreenActivity.java
@@ -1,25 +1,141 @@
 package com.app.manuel.flashlight;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 public class FullscreenActivity extends AppCompatActivity {
 
+    ToggleButton onbtn;
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private Boolean isTorchOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
 
+        //onbtn.setBackgroundResource(R.drawable.switchoff);
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        onbtn = (ToggleButton) findViewById(R.id.toggleButton);
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        isTorchOn = false;
+
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isFlashAvailable) {
+
+            AlertDialog alert = new AlertDialog.Builder(FullscreenActivity.this)
+                    .create();
+            alert.setTitle("Error !!");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // closing the application
+                            finish();
+                            System.exit(0);
+                        }
+                    });
+            alert.show();
+            return;
+        }
+
+
+
+
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        onbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (isTorchOn) {
+                        turnOffFlashLight();
+                        isTorchOn = false;
+                    } else {
+                        turnOnFlashLight();
+                        isTorchOn = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void turnOnFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, true);
+
+                //mTorchOnOffButton.setImageResource(R.drawable.on);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
+    public void turnOffFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
+                // mTorchOnOffButton.setImageResource(R.drawable.off);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isTorchOn){
+            //turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(isTorchOn){
+            //turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isTorchOn){
+            turnOnFlashLight();
+        }
+    }
 
 }
